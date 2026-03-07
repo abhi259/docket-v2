704480d123639efe67682f10e1fdc216bcad3d01
Abhishek Patil <abhishekpatil925@gmail.com>
2026-03-08 00:30:44 +0530

    Made searchbar  sticky

--- src/app/_components/body/Body.tsx
+++ src/app/_components/body/Body.tsx
@@ -3,10 +3,14 @@ import SearchBar from "./SearchBar";
 
 export default function Body() {
   return (
-    <div className="w-full h-full overflow-scroll p-10 ">
-      <SearchBar />
-      <p className="text-2xl font-bold p-4">Our Menu</p>
-      <Menu />
+    <div className="w-full h-full flex flex-col">
+      <div className="sticky top-0 bg-white z-10 px-10 pt-10">
+        <SearchBar />
+      </div>
+      <div className="flex-1 overflow-scroll px-10 pb-10">
+        <p className="text-2xl font-bold p-4">Our Menu</p>
+        <Menu />
+      </div>
     </div>
   );
 }
